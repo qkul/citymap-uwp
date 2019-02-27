8aac4634da141d88d44a7a207d4cc28a3b62748f
q_kul <q_kul@mail.ru>
2019-02-27 11:04:34 +0300

    part1

---
+++ CityMapUWP/Views/CitiesView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CityMapUWP.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CityMapUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CitiesView : Page
+    {
+        public CitiesView()
+        {
+            this.InitializeComponent();
+
+            var citiesService = new CitiesService();
+            CitiesListView.ItemsSource = citiesService.Cities;
+        }
+
+        private void CitiesListView_ItemClick(object sender, ItemClickEventArgs itemClickEvent)
+        {
+            Frame.Navigate(typeof(CityDetailsView), itemClickEvent.ClickedItem);
+        }
+    }
+}
---
+++ CityMapUWP/Views/CityDetailsView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CityMapUWP.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CityMapUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CityDetailsView : Page
+    {
+        public CityDetailsView()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var city = e.Parameter as City;
+
+            if (city != null)
+            {
+                NameTextBlock.Text = city.Name;
+                DescriptionTextBlock.Text = city.Description;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
